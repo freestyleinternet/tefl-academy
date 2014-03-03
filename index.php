4bf3add1ac56ccf4983adb0d0e224b77190d7c45
freestyle <annemarie@amparsons.co.uk>
2014-03-03 17:26:14 +0000

    File structure update

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+    <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+    <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+    <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+    <!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+    <!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
+    <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]--><head>
+            
+    <title></title>
+    <!--[if lt IE 9]>
+    <script src="js/vendor//html5shiv.js"></script>
+    <![endif]-->
+    <meta name="description" content="">
+    <!--<meta name="viewport" content="user-scalable=yes;width=device-width;initial-scale=0.31; maximum-scale=1.0; minimum-scale=0.25">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0">-->
+    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+     <meta name="viewport" content="user-scalable=yes;width=device-width;initial-scale=0.31; maximum-scale=1.0; minimum-scale=0.25">
+    
+    <link rel="shortcut icon" href="favicon.ico">
+    <!-- For third-generation iPad with high-resolution Retina display: -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144-precomposed.png">
+    <!-- For iPhone with high-resolution Retina display: -->
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114-precomposed.png">
+    <!-- For first- and second-generation iPad: -->
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72-precomposed.png">
+    <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+    <link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+    
+    <link href="https://get.gridsetapp.com/27276/" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css">
+	<script src="assets/js/vendor/modernizr-2.6.2.min.js"></script>
+</head>
+
+<body>
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+    
+    <!--<header>
+    	<div class="wrapper">
+        	<p>Header</p>
+        </div>
+    </header>-->
+    
+    <div class="wrapper">
+    	<div class="m1 t1 d1 box">
+        	<p>asdasdasd</p>
+        </div>
+        <div class="m1 t2 d2 box">
+        	<p>asdasdasd</p>
+        </div>
+        <div class="m1 t3 d3 box">
+       		<p>asdasdasd</p>
+        </div>
+        <div class="m1 t4 d4 box">
+       		<p>asdasdasd</p>
+        </div>
+    </div>
+    
+    <!--<section class="banner">
+    	<div class="wrapper">
+        	<p>Banner</p>
+        </div>
+    </section>
+    
+    <footer>
+    	<div class="wrapper">
+        	<p>Footer</p>
+        </div>
+    </footer>-->
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+    <script src="assets/js/plugins.min.js"></script>
+    <script src="assets/js/main.min.js"></script>
+    
+    <script>
+        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
+</body>
+</html>
