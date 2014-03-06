32e55f3198185f84ece17e095a3899914f7ab75a
freestyle <annemarie@amparsons.co.uk>
2014-03-06 09:35:48 +0000

    Home page built

--- assets/js/source/main.js
+++ assets/js/source/main.js
@@ -1,5 +1,13 @@
 (function($) {
 
+$( 'a[href="#"]' ).click( function(e) {
+	e.preventDefault();
+} );
 
+$('.toggle').click(function()
+{
+	$('div.mobile').animate({height: 'toggle'}, 200);
+	return false;
+});
 
 })(jQuery);
