1b9b4703c80e64adef29b178766ab40b17dfc25d
freestyle <annemarie@amparsons.co.uk>
2014-03-06 13:56:35 +0000

    news

---
+++ news-single.php
@@ -0,0 +1,46 @@
+<?php include 'includes/header.php'; ?>
+    
+    <div class="wrapper">
+    	
+      	<section class="m1 t1-t3 d1-d3">
+        	
+            <div class="news-intro single">
+            	<h1>THE TEFL ACADEMY LAUNCHES IN HULL!</h1>
+            	<p>posted <span>18 Dec 2013</span></p>
+            </div>
+            
+            <h1>Word on the grapevine is that Hull is becoming the City of Culture!</h1>
+ 
+        </section>
+        
+        <aside class="m1 t4 d4">
+        	<div class="home-feature nocontent">
+       			<img src="assets/images/feature3.svg" alt="TEFL Academy"/>
+                <h2>Find your<br>Local course</h2>
+                <h3>UK wide courses<br>every month</h3>
+            	<a class="button-light oarrow absolutebr" href="#">Search<span></span></a>
+            </div>
+            <div class="home-feature nocontent">
+            	<img src="assets/images/feature4.jpg" alt="TEFL Academy"/>
+                <h2>Download 2013<br>Prospectus</h2>
+            	<a class="button-light oarrow absolutebr" href="#">Download<span></span></a>
+            </div>
+            <div class="home-feature nocontent special">
+       			<img src="assets/images/feature5.svg" alt="TEFL Academy"/>
+                <h2>Bring a friend <br>&amp; save £50!</h2>
+                <h3>Book 2 places at the<br>same time &amp; get £50 off</h3>
+            	<a class="button-light oarrow absolutebr" href="#">Book Now<span></span></a>
+            </div>
+            <div class="home-feature nocontent hbg">
+            	<img src="assets/images/feature6.jpg" alt="TEFL Academy"/>
+                <h2>Schools <br>Programme</h2>
+            	<a class="button-light oarrow absolutebr" href="#">More Info<span></span></a>
+            </div>
+        </aside>
+        
+    </div>
+    
+	<?php include 'includes/testimonials.php'; ?>
+    <?php include 'includes/social-section.php'; ?>
+    
+<?php include 'includes/footer.php'; ?>
---
+++ news.php
@@ -0,0 +1,108 @@
+<?php include 'includes/header.php'; ?>
+    
+    <div class="wrapper">
+    	
+      	<section class="m1 t1-t3 d1-d3">
+        	
+            <div class="news-intro">
+            	<h1>ALL THE LATEST NEWS</h1>
+            	<p>As one of the fastest growing TEFL course providers in the UK we have lots of news to share with our students. Get all the latest news right here!</p>
+            </div>
+            
+            <div class="container">
+                <article>
+                    <a href="#">
+                        <img src="assets/images/blog-thumbnail.jpg" alt="TEFL Academy"/>
+                        <h1>THE TEFL ACADEMY LAUNCHES IN HULL!</h1>
+                        <p>Word on the grapevine is that Hull is becoming the City of Culture!  With the variety of venues and events, from ...</p>
+                        <p class="datetime">18 Dec 2013</p>
+                        <div class="button-light oarrow floatright" href="#">More<span></span></div>
+                    </a>
+                </article>
+                <article>
+                    <a href="#">
+                        <img src="assets/images/blog-thumbnail.jpg" alt="TEFL Academy"/>
+                        <h1>THE TEFL ACADEMY LAUNCHES IN HULL!</h1>
+                        <p>Word on the grapevine is that Hull is becoming the City of Culture!  With the variety of venues and events, from ...</p>
+                        <p class="datetime">18 Dec 2013</p>
+                        <div class="button-light oarrow floatright" href="#">More<span></span></div>
+                    </a>
+                </article>
+                <article>
+                    <a href="#">
+                        <img src="assets/images/blog-thumbnail.jpg" alt="TEFL Academy"/>
+                        <h1>THE TEFL ACADEMY LAUNCHES IN HULL!</h1>
+                        <p>Word on the grapevine is that Hull is becoming the City of Culture!  With the variety of venues and events, from ...</p>
+                        <p class="datetime">18 Dec 2013</p>
+                        <div class="button-light oarrow floatright" href="#">More<span></span></div>
+                    </a>
+                </article>
+                <article>
+                    <a href="#">
+                        <img src="assets/images/blog-thumbnail.jpg" alt="TEFL Academy"/>
+                        <h1>THE TEFL ACADEMY LAUNCHES IN HULL!</h1>
+                        <p>Word on the grapevine is that Hull is becoming the City of Culture!  With the variety of venues and events, from ...</p>
+                        <p class="datetime">18 Dec 2013</p>
+                        <div class="button-light oarrow floatright" href="#">More<span></span></div>
+                    </a>
+                </article>
+                <article>
+                    <a href="#">
+                        <img src="assets/images/blog-thumbnail.jpg" alt="TEFL Academy"/>
+                        <h1>THE TEFL ACADEMY LAUNCHES IN HULL!</h1>
+                        <p>Word on the grapevine is that Hull is becoming the City of Culture!  With the variety of venues and events, from ...</p>
+                        <p class="datetime">18 Dec 2013</p>
+                        <div class="button-light oarrow floatright" href="#">More<span></span></div>
+                    </a>
+                </article>
+                <div class="pagination">
+                	<ul>
+                    	<li><a href="#">&lt; Prev</a></li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li><a href="#">6</a></li>
+                        <li><a href="#">7</a></li>
+                        <li><a href="#">8</a></li>
+                        <li><a href="#">9</a></li>
+                        <li><a href="#">10</a></li>
+                        <li><a href="#">Next &gt;</a></li>
+                    </ul>	
+                </div>
+            </div>
+            
+        </section>
+        
+        <aside class="m1 t4 d4">
+        	<div class="home-feature nocontent">
+       			<img src="assets/images/feature3.svg" alt="TEFL Academy"/>
+                <h2>Find your<br>Local course</h2>
+                <h3>UK wide courses<br>every month</h3>
+            	<a class="button-light oarrow absolutebr" href="#">Search<span></span></a>
+            </div>
+            <div class="home-feature nocontent">
+            	<img src="assets/images/feature4.jpg" alt="TEFL Academy"/>
+                <h2>Download 2013<br>Prospectus</h2>
+            	<a class="button-light oarrow absolutebr" href="#">Download<span></span></a>
+            </div>
+            <div class="home-feature nocontent special">
+       			<img src="assets/images/feature5.svg" alt="TEFL Academy"/>
+                <h2>Bring a friend <br>&amp; save £50!</h2>
+                <h3>Book 2 places at the<br>same time &amp; get £50 off</h3>
+            	<a class="button-light oarrow absolutebr" href="#">Book Now<span></span></a>
+            </div>
+            <div class="home-feature nocontent hbg">
+            	<img src="assets/images/feature6.jpg" alt="TEFL Academy"/>
+                <h2>Schools <br>Programme</h2>
+            	<a class="button-light oarrow absolutebr" href="#">More Info<span></span></a>
+            </div>
+        </aside>
+        
+    </div>
+    
+	<?php include 'includes/testimonials.php'; ?>
+    <?php include 'includes/social-section.php'; ?>
+    
+<?php include 'includes/footer.php'; ?>
