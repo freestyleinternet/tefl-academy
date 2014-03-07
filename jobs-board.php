a38a05cf1b08f5590a9f6c488d7c88c03a0a241b
freestyle <annemarie@amparsons.co.uk>
2014-03-07 12:21:13 +0000

    jobs board desktop page

---
+++ jobs-board.php
@@ -0,0 +1,136 @@
+<?php include 'includes/header.php'; ?>
+    
+    <div class="wrapper">
+    	
+      	<section class="m1 t1-t3 d1-d3">
+        	
+            <div class="news-intro jobs">
+            	<h1><img src="assets/images/mag-glass-green.svg" alt="mag glass"/>TEFL JOBS BOARD - Search 364 jobs</h1>
+            	<p>On successful completion of our 120 hour TEFL course you can start applying for jobs on our exclusive jobs board. Remember to check back regularly, new jobs are posted daily.</p>
+            	<form>
+                	<fieldset class="half">
+                        <input type="text" placeholder="Keywords">
+                        <div class="styled floatright">
+                           <select>
+                              <option>Location</option>
+                              <option>Option one</option>
+                              <option>Option two</option>
+                              <option>Option three</option>
+                           </select>
+                        </div>
+                    </fieldset>
+                    <fieldset class="third">
+                    	<div class="styled">
+                           <select>
+                              <option>Full Time</option>
+                              <option>Option one</option>
+                              <option>Option two</option>
+                              <option>Option three</option>
+                           </select>
+                        </div>
+                        <div class="styled">
+                           <select>
+                              <option>Start Date</option>
+                              <option>Option one</option>
+                              <option>Option two</option>
+                              <option>Option three</option>
+                           </select>
+                        </div>
+                        <input type="submit" value="Search">
+                    </fieldset>
+                </form>
+            </div>
+            
+            <div class="container-jobs">
+               	
+                <div class="head"> 
+                	<h1>JOB SUMMARY</h1>
+                    <div class="styled">
+                        <select>
+                            <option>Full Time</option>
+                            <option>Option one</option>
+                            <option>Option two</option>
+                            <option>Option three</option>
+                        </select>
+                    </div>
+                </div>
+                
+                <article>
+                	<a href="#">
+                    	<header>
+                            <h1>Brilliant teaching Opportunities in Thailand</h1>
+                            <p class="location">Thailand</p>
+                            <p class="datetime">15/12/13</p>
+                        </header>
+                        <img src="assets/images/flag.svg" alt="TEFL Academy"/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation .</p>
+                        <div class="button-light oarrow absolutebr">More Info<span></span></div>
+                    </a>
+                </article>
+                <article>
+                	<a href="#">
+                    	<header>
+                            <h1>Brilliant teaching Opportunities in Thailand</h1>
+                            <p class="location">Thailand</p>
+                            <p class="datetime">15/12/13</p>
+                        </header>
+                        <img src="assets/images/flag.svg" alt="TEFL Academy"/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation .</p>
+                        <div class="button-light oarrow absolutebr">More Info<span></span></div>
+                    </a>
+                </article>
+                <article>
+                	<a href="#">
+                    	<header>
+                            <h1>Brilliant teaching Opportunities in Thailand</h1>
+                            <p class="location">Thailand</p>
+                            <p class="datetime">15/12/13</p>
+                        </header>
+                        <img src="assets/images/flag.svg" alt="TEFL Academy"/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation .</p>
+                        <div class="button-light oarrow absolutebr">More Info<span></span></div>
+                    </a>
+                </article>
+                
+                <div class="pagination-general">
+                	<ul>
+                    	<li><a href="#">&lt; Prev</a></li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li><a href="#">6</a></li>
+                        <li><a href="#">7</a></li>
+                        <li><a href="#">8</a></li>
+                        <li><a href="#">9</a></li>
+                        <li><a href="#">10</a></li>
+                        <li><a href="#">Next &gt;</a></li>
+                    </ul>	
+                </div>
+                 
+            </div>
+            
+        </section>
+        
+        <aside class="m1 t4 d4">
+        	
+            <div class="newsletter-box">
+                <img src="assets/images/newsletter-block-header.jpg">
+                <h1>SIGN UP TO OUR NEWSLETTER</h1>
+                <p>Be the first to get exclusive TEFL offers and information</p>
+                <form action="#" method="post">
+                    <input type="text" placeholder="Name" required>
+                    <input  type="email" placeholder="Email" required>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+            
+            <a class="button-light mobbutton orange" href="#">Most Recent Jobs</a>
+            
+        </aside>
+        
+    </div>
+
+    
+<?php include 'includes/footer.php'; ?>
