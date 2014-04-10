d3770348fa5b0e683c512692a1fd75b630457317
freestyle <annemarie@amparsons.co.uk>
2014-04-10 13:45:41 +0100

    new pages

---
+++ accreditation.php
@@ -0,0 +1,69 @@
+<?php include 'includes/header.php'; ?>
+    
+    <div class="wrapper">
+    	
+      	<section class="m1 t-all d1-d3">
+        	
+            <div class="news-intro autoheight world">
+            	<h1>ACCREDITATION</h1>
+            	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam imperdiet purus ac quam tincidunt, vel hendrerit nunc fermentum. Phasellus iaculis ullamcorper leo at congue. Phasellus iaculis ullamcorper leo at congue.</p>
+            </div>
+
+            <div class="bodycopy fullwidth">
+            	<!--<a href="#"><img class="floatright" src="assets/images/meet-the-trainers.jpg" alt="TEFL Academy"/></a>-->
+                <img class="floatright" src="assets/images/wtefac.svg">
+            	<p>When choosing a TEFL course you should firstly ensure that it is accredited by an accreditation body. Potential TEFL course students should remember that their prospective employers are likely to be based outside of the UK. This means that accreditation bodies based in the UK may not be recognised by employers abroad. This is why The TEFL Academy is fully accredited by the World TEFL Accrediting Commission (WTEFLAC). The WTEFLAC is an international body that accredits and regulates TEFL courses.</p>
+            	<p>WTEFLAC registration no. theteflacademy465764UK83</p>
+                <p>For further information about the WTEFLAC please visit their website: <a href="http://www.wteflac.com">www.wteflac.com</a></p>
+            </div>
+            
+            <a class="button-light mobbutton displaymobile" href="#">Call back request</a>
+        	<a class="button-light mobbutton orange displaymobile" href="#">50% Off <span></span></a>
+
+        </section>
+        
+        <aside class="m1 t-all d4">
+        	<div class="m1 t1 d-all">
+                <div class="home-feature nocontent">
+					<a href="#">
+                        <img src="assets/images/feature3.svg" alt="TEFL Academy"/>
+                        <h2>Find your<br>Local course</h2>
+                        <h3>UK wide courses<br>every month</h3>
+                        <div class="button-light oarrow absolutebr" href="#">Search<span></span></div>
+                    </a>
+                </div>
+            </div>
+            <div class="m1 t2 d-all">
+                <div class="home-feature nocontent">
+                    <a href="#">
+                        <img src="assets/images/feature4.jpg" alt="TEFL Academy"/>
+                        <h2>Download 2014<br>Prospectus</h2>
+                        <div class="button-light oarrow absolutebr" href="#">Download<span></span></div>
+                    </a>
+                </div>
+            </div>
+            <div class="m1 t3 d-all">
+                <div class="home-feature nocontent special">
+                    <a href="#">
+                        <img src="assets/images/feature5.svg" alt="TEFL Academy"/>
+                        <h2>Bring a friend <br>&amp; save £50!</h2>
+                        <h3>Book 2 places at the<br>same time &amp; get <strong>£50 off</strong></h3>
+                        <div class="button-light oarrow absolutebr" href="#">Book Now<span></span></div>
+                    </a>
+                </div>
+            </div>
+            <div class="m1 t4 d-all">
+                <div class="home-feature nocontent hbg">
+                    <a href="#">
+                        <img src="assets/images/feature6.jpg" alt="TEFL Academy"/>
+                        <h2>Schools <br>Programme</h2>
+                        <div class="button-light oarrow absolutebr" href="#">More Info<span></span></div>
+                    </a>
+                </div>
+            </div>
+        </aside>
+        
+    </div>
+    <?php include 'includes/social-section.php'; ?>
+    
+<?php include 'includes/footer.php'; ?>
---
+++ contact-us.php
@@ -0,0 +1,109 @@
+<?php include 'includes/header.php'; ?>
+    
+    <div class="wrapper">
+    	
+      	<section class="m1 t-all d1-d3">
+        	
+            <div class="news-intro autoheight world">
+            	<h1>CONTACT US</h1>
+            	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam imperdiet purus ac quam tincidunt, vel hendrerit nunc fermentum. Phasellus iaculis ullamcorper leo at congue. Phasellus iaculis ullamcorper leo at congue.</p>
+            </div>
+
+            <div class="bodycopy fullwidth">
+            	<div class="col-split-1">
+                    <h2>Main Switchboard:</h2>
+                    <p>0800 689 9190</p>
+                    <h2>Lines Open:</h2>
+                    <p>9am - 6pm, Sat: 8am - 5pm, Sunday: Closed</p>
+                    <h2>Email Contact:</h2>
+                    <p>
+                        General Enquiries: <a href="mailto:info@theteflacademy.co.uk">info@theteflacademy.co.uk</a><br>
+                        Recruitment:<a href="mailto:recruitment@theteflacademy.co.uk"> recruitment@theteflacademy.co.uk</a><br>
+                        Accounts:<a href="mailto:accounts@theteflacademy.co.uk"> accounts@theteflacademy.co.uk</a><br>
+                        Press Enquiries:<a href="mailto:press@theteflacademy.co.uk"> press@theteflacademy.co.uk</a>
+                    </p>
+                    <h2>Postal Address:</h2>
+                    <p>
+                        The TEFL Academy <br>
+                        Suite 10,&nbsp;<br>
+                        7 Marlborough Place,<br>
+                        Brighton,&nbsp;<br>
+                        BN1 1UB
+                    </p>
+                </div>
+                
+                <div class="col-split-2">
+                    <h2>SEND A MESSAGE</h2>
+                    <form action="#" method="post">
+                        <div>
+                            <label>Name</label>
+                            <input type="text">
+                        </div>
+                        <div>
+                            <label>Email</label>
+                            <input type="email">
+                        </div>
+                        <div>
+                            <label>Telephone</label>
+                            <input type="text">
+                        </div>
+                        <div>
+                            <label>Message</label>
+                            <textarea></textarea>
+                        </div>
+                        <input class="buttontype orange" type="submit" value="Submit">
+                    </form>
+                </div>
+                
+            </div>
+            
+            <a class="button-light mobbutton displaymobile" href="#">Call back request</a>
+        	<a class="button-light mobbutton orange displaymobile" href="#">50% Off <span></span></a>
+
+        </section>
+        
+        <aside class="m1 t-all d4">
+        	<div class="m1 t1 d-all">
+                <div class="home-feature nocontent">
+					<a href="#">
+                        <img src="assets/images/feature3.svg" alt="TEFL Academy"/>
+                        <h2>Find your<br>Local course</h2>
+                        <h3>UK wide courses<br>every month</h3>
+                        <div class="button-light oarrow absolutebr" href="#">Search<span></span></div>
+                    </a>
+                </div>
+            </div>
+            <div class="m1 t2 d-all">
+                <div class="home-feature nocontent">
+                    <a href="#">
+                        <img src="assets/images/feature4.jpg" alt="TEFL Academy"/>
+                        <h2>Download 2014<br>Prospectus</h2>
+                        <div class="button-light oarrow absolutebr" href="#">Download<span></span></div>
+                    </a>
+                </div>
+            </div>
+            <div class="m1 t3 d-all">
+                <div class="home-feature nocontent special">
+                    <a href="#">
+                        <img src="assets/images/feature5.svg" alt="TEFL Academy"/>
+                        <h2>Bring a friend <br>&amp; save £50!</h2>
+                        <h3>Book 2 places at the<br>same time &amp; get <strong>£50 off</strong></h3>
+                        <div class="button-light oarrow absolutebr" href="#">Book Now<span></span></div>
+                    </a>
+                </div>
+            </div>
+            <div class="m1 t4 d-all">
+                <div class="home-feature nocontent hbg">
+                    <a href="#">
+                        <img src="assets/images/feature6.jpg" alt="TEFL Academy"/>
+                        <h2>Schools <br>Programme</h2>
+                        <div class="button-light oarrow absolutebr" href="#">More Info<span></span></div>
+                    </a>
+                </div>
+            </div>
+        </aside>
+        
+    </div>
+    <?php include 'includes/social-section.php'; ?>
+    
+<?php include 'includes/footer.php'; ?>
