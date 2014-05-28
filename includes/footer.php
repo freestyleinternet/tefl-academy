70e44abd4866137b074d3f7e5cb6d786dc54f1b4
freestyle <annemarie@amparsons.co.uk>
2014-05-28 15:35:55 +0100

    New arrow and button for course locations page

--- includes/footer.php
+++ includes/footer.php
@@ -148,7 +148,7 @@
 											  address:val.address,
 											  //closeBoxURL: "assets/images/cross-infowindow.png",
 											  options:{
-												  content:'<div class="infobox" id="infoBox-"'+val.id+'"><h1>'+val.title+'<a href="#" class="close" onclick="javascript:closeInfo(this); return false;"></a> </h1><div id="bodyContent"><p class="address">'+val.address+'</p><p class="phone" ><img src="assets/images/small-phone-icon.svg" alt="TEFL Academy Phone Number"/> <span>'+val.phone+'</span></p><p><a href="mailto:'+val.emailad+'"> '+val.emailad+'</a></p><p><a href="'+val.pagelink+'"><img src="assets/images/more-info-button.svg" alt="More Info"/></a></p><div class="downarrow"></div></div></div>',
+												  content:'<div class="infobox" id="infoBox-"'+val.id+'"><h1>'+val.title+'<a href="#" class="close" onclick="javascript:closeInfo(this); return false;"></a> </h1><div id="bodyContent"><p class="address">'+val.address+'</p><p class="phone" ><img src="assets/images/small-phone-icon.svg" alt="TEFL Academy Phone Number"/> <span>'+val.phone+'</span></p><p><a href="mailto:'+val.emailad+'"> '+val.emailad+'</a></p><p><a href="'+val.pagelink+'"><img class="buttontoinfo" src="assets/images/more-info-button.svg" alt="More Info"/></a></p><div class="downarrow"></div></div></div>',
 												  
 												   
 												   offset:{
